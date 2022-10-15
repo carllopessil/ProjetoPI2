33fd6d07a54df3242d9eb861269cd9f456ffca65
HeloeJoao <marsa@DESKTOP-1MCKGM2>
2022-10-15 20:58:55 -0300

    Classe - Buscar Cliente

---
+++ ProjetoPi2Semestre2/src/main/java/com/mycompany/projetopi2semestre2/BuscarCliente.java
@@ -0,0 +1,116 @@
+
+package com.mycompany.projetopi2semestre2;
+
+public class BuscarCliente {
+   private static String txtNomeBuscar = "";
+   private static String txtCPFBuscar = "";
+   private static String txtNomeCliente = "";
+   private static int txtCPFCliente;
+   private static int txtDataNascimentoCliente;
+   private static String cbEstadoCivilCliente = "";
+   private static String txtEmailCliente = "";
+   private static String cbSexoCliente = "";
+   private static int txtCEPCliente;
+   private static int txtEnderecoCliente;
+   private static int txtNumeroCliente;
+   private static String txtComplementoCliente = "";
+
+    public static String getTxtNomeBuscar() {
+        return txtNomeBuscar;
+    }
+
+    public static void setTxtNomeBuscar(String txtNomeBuscar) {
+        BuscarCliente.txtNomeBuscar = txtNomeBuscar;
+    }
+
+    public static String getTxtCPFBuscar() {
+        return txtCPFBuscar;
+    }
+
+    public static void setTxtCPFBuscar(String txtCPFBuscar) {
+        BuscarCliente.txtCPFBuscar = txtCPFBuscar;
+    }
+
+    public static String getTxtNomeCliente() {
+        return txtNomeCliente;
+    }
+
+    public static void setTxtNomeCliente(String txtNomeCliente) {
+        BuscarCliente.txtNomeCliente = txtNomeCliente;
+    }
+
+    public static int getTxtCPFCliente() {
+        return txtCPFCliente;
+    }
+
+    public static void setTxtCPFCliente(int txtCPFCliente) {
+        BuscarCliente.txtCPFCliente = txtCPFCliente;
+    }
+
+    public static int getTxtDataNascimentoCliente() {
+        return txtDataNascimentoCliente;
+    }
+
+    public static void setTxtDataNascimentoCliente(int txtDataNascimentoCliente) {
+        BuscarCliente.txtDataNascimentoCliente = txtDataNascimentoCliente;
+    }
+
+    public static String getCbEstadoCivilCliente() {
+        return cbEstadoCivilCliente;
+    }
+
+    public static void setCbEstadoCivilCliente(String cbEstadoCivilCliente) {
+        BuscarCliente.cbEstadoCivilCliente = cbEstadoCivilCliente;
+    }
+
+    public static String getTxtEmailCliente() {
+        return txtEmailCliente;
+    }
+
+    public static void setTxtEmailCliente(String txtEmailCliente) {
+        BuscarCliente.txtEmailCliente = txtEmailCliente;
+    }
+
+    public static String getCbSexoCliente() {
+        return cbSexoCliente;
+    }
+
+    public static void setCbSexoCliente(String cbSexoCliente) {
+        BuscarCliente.cbSexoCliente = cbSexoCliente;
+    }
+
+    public static int getTxtCEPCliente() {
+        return txtCEPCliente;
+    }
+
+    public static void setTxtCEPCliente(int txtCEPCliente) {
+        BuscarCliente.txtCEPCliente = txtCEPCliente;
+    }
+
+    public static int getTxtEnderecoCliente() {
+        return txtEnderecoCliente;
+    }
+
+    public static void setTxtEnderecoCliente(int txtEnderecoCliente) {
+        BuscarCliente.txtEnderecoCliente = txtEnderecoCliente;
+    }
+
+    public static int getTxtNumeroCliente() {
+        return txtNumeroCliente;
+    }
+
+    public static void setTxtNumeroCliente(int txtNumeroCliente) {
+        BuscarCliente.txtNumeroCliente = txtNumeroCliente;
+    }
+
+    public static String getTxtComplementoCliente() {
+        return txtComplementoCliente;
+    }
+
+    public static void setTxtComplementoCliente(String txtComplementoCliente) {
+        BuscarCliente.txtComplementoCliente = txtComplementoCliente;
+    }
+
+    public BuscarCliente() {
+    }
+}
