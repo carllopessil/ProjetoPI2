dd14cd40ea57807d729e43676caa7f3daa0245c7
HeloeJoao <>
2022-10-15 21:15:35 -0300

    Classes - vendedor

---
+++ ProjetoPi2Semestre2/src/main/java/com/mycompany/projetopi2semestre2/CadastroDoVendedor.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetopi2semestre2;
+
+
+public class CadastroDoVendedor {
+    private static String txtNomeVend = "";
+    private static int txtCPFVend;
+    private static int txtDataNascimentoVend;
+    private static String cbEstadoCivilVend = "";
+    private static String txtEmailVend = "";
+    private static String cbSexovend = "";
+    private static int txtTelefoneVend;
+    private static int txtCEPVend;
+    private static String txtEnderecoVend = "";
+    private static int txtNumeroVend;
+    private static String txtComplementoVend = "";
+
+    public CadastroDoVendedor() {
+    }
+
+    public static String getTxtNomeVend() {
+        return txtNomeVend;
+    }
+
+    public static void setTxtNomeVend(String txtNomeVend) {
+        CadastroDoVendedor.txtNomeVend = txtNomeVend;
+    }
+
+    public static int getTxtCPFVend() {
+        return txtCPFVend;
+    }
+
+    public static void setTxtCPFVend(int txtCPFVend) {
+        CadastroDoVendedor.txtCPFVend = txtCPFVend;
+    }
+
+    public static int getTxtDataNascimentoVend() {
+        return txtDataNascimentoVend;
+    }
+
+    public static void setTxtDataNascimentoVend(int txtDataNascimentoVend) {
+        CadastroDoVendedor.txtDataNascimentoVend = txtDataNascimentoVend;
+    }
+
+    public static String getCbEstadoCivilVend() {
+        return cbEstadoCivilVend;
+    }
+
+    public static void setCbEstadoCivilVend(String cbEstadoCivilVend) {
+        CadastroDoVendedor.cbEstadoCivilVend = cbEstadoCivilVend;
+    }
+
+    public static String getTxtEmailVend() {
+        return txtEmailVend;
+    }
+
+    public static void setTxtEmailVend(String txtEmailVend) {
+        CadastroDoVendedor.txtEmailVend = txtEmailVend;
+    }
+
+    public static String getCbSexovend() {
+        return cbSexovend;
+    }
+
+    public static void setCbSexovend(String cbSexovend) {
+        CadastroDoVendedor.cbSexovend = cbSexovend;
+    }
+
+    public static int getTxtTelefoneVend() {
+        return txtTelefoneVend;
+    }
+
+    public static void setTxtTelefoneVend(int txtTelefoneVend) {
+        CadastroDoVendedor.txtTelefoneVend = txtTelefoneVend;
+    }
+
+    public static int getTxtCEPVend() {
+        return txtCEPVend;
+    }
+
+    public static void setTxtCEPVend(int txtCEPVend) {
+        CadastroDoVendedor.txtCEPVend = txtCEPVend;
+    }
+
+    public static String getTxtEnderecoVend() {
+        return txtEnderecoVend;
+    }
+
+    public static void setTxtEnderecoVend(String txtEnderecoVend) {
+        CadastroDoVendedor.txtEnderecoVend = txtEnderecoVend;
+    }
+
+    public static int getTxtNumeroVend() {
+        return txtNumeroVend;
+    }
+
+    public static void setTxtNumeroVend(int txtNumeroVend) {
+        CadastroDoVendedor.txtNumeroVend = txtNumeroVend;
+    }
+
+    public static String getTxtComplementoVend() {
+        return txtComplementoVend;
+    }
+
+    public static void setTxtComplementoVend(String txtComplementoVend) {
+        CadastroDoVendedor.txtComplementoVend = txtComplementoVend;
+    }
+
+}
