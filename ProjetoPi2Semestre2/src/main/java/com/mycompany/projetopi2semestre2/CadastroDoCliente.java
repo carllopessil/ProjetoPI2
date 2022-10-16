c9d5fc9bf2a1d5b40e16f05dd3e29e566bb63ed2
HeloeJoao <>
2022-10-15 21:17:14 -0300

    Classes - cliente

---
+++ ProjetoPi2Semestre2/src/main/java/com/mycompany/projetopi2semestre2/CadastroDoCliente.java
@@ -0,0 +1,109 @@
+
+package com.mycompany.projetopi2semestre2;
+
+
+public class CadastroDoCliente {
+    private static String txtNome = "";
+    private static int txtCPF;
+    private static int txtDataNascimento;
+    private static String cbEstadoCivil = "";
+    private static String txtEmail = "";
+    private static String cbSexo = "";
+    private static int txtTelefone;
+    private static int txtCEP;
+    private static String txtEndereco = "";
+    private static int txtNumero;
+    private static String txtComplemento = "";
+
+    public CadastroDoCliente() {
+    }
+
+    public static String getTxtNome() {
+        return txtNome;
+    }
+
+    public static void setTxtNome(String txtNome) {
+        CadastroDoCliente.txtNome = txtNome;
+    }
+
+    public static int getTxtCPF() {
+        return txtCPF;
+    }
+
+    public static void setTxtCPF(int txtCPF) {
+        CadastroDoCliente.txtCPF = txtCPF;
+    }
+
+    public static int getTxtDataNascimento() {
+        return txtDataNascimento;
+    }
+
+    public static void setTxtDataNascimento(int txtDataNascimento) {
+        CadastroDoCliente.txtDataNascimento = txtDataNascimento;
+    }
+
+    public static String getCbEstadoCivil() {
+        return cbEstadoCivil;
+    }
+
+    public static void setCbEstadoCivil(String cbEstadoCivil) {
+        CadastroDoCliente.cbEstadoCivil = cbEstadoCivil;
+    }
+
+    public static String getTxtEmail() {
+        return txtEmail;
+    }
+
+    public static void setTxtEmail(String txtEmail) {
+        CadastroDoCliente.txtEmail = txtEmail;
+    }
+
+    public static String getCbSexo() {
+        return cbSexo;
+    }
+
+    public static void setCbSexo(String cbSexo) {
+        CadastroDoCliente.cbSexo = cbSexo;
+    }
+
+    public static int getTxtTelefone() {
+        return txtTelefone;
+    }
+
+    public static void setTxtTelefone(int txtTelefone) {
+        CadastroDoCliente.txtTelefone = txtTelefone;
+    }
+
+    public static int getTxtCEP() {
+        return txtCEP;
+    }
+
+    public static void setTxtCEP(int txtCEP) {
+        CadastroDoCliente.txtCEP = txtCEP;
+    }
+
+    public static String getTxtEndereco() {
+        return txtEndereco;
+    }
+
+    public static void setTxtEndereco(String txtEndereco) {
+        CadastroDoCliente.txtEndereco = txtEndereco;
+    }
+
+    public static int getTxtNumero() {
+        return txtNumero;
+    }
+
+    public static void setTxtNumero(int txtNumero) {
+        CadastroDoCliente.txtNumero = txtNumero;
+    }
+
+    public static String getTxtComplemento() {
+        return txtComplemento;
+    }
+
+    public static void setTxtComplemento(String txtComplemento) {
+        CadastroDoCliente.txtComplemento = txtComplemento;
+    }
+    
+}
