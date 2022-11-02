123d2a8de62ee24893013d4c0dcd0b32399ea145
João Victor <jvsantana2013@gmail.com>
2022-11-02 12:08:52 -0300

    Classes construtores dos Produtos

---
+++ ProjetoPi2Semestre2/src/main/java/com/mycompany/projetopi2semestre2/BuscarDoProduto.java
@@ -0,0 +1,61 @@
+
+package com.mycompany.projetopi2semestre2;
+
+
+public class BuscarDoProduto {
+    private static String txtCodBarras = "";
+    private static String cboCategorias= "";
+    private static String cboTamanho = "";
+    private static String cboPreco = "";
+    private static String cboMarca = "";
+
+
+//Codigo de Barras
+    public static String getCodBarras() {
+        return txtCodBarras;
+    }
+
+    public static void setCodBarras(String txtcodBarras) {
+        BuscarDoProduto.txtCodBarras=txtcodBarras;
+    }
+
+
+//Categorias 
+    public static String getCategorias() {
+        return cboCategorias;
+    }
+     public static void setCategorias(String cboCategorias) {
+        BuscarDoProduto.cboCategorias=cboCategorias;
+    }
+
+
+//Tamanho
+    public static String getTamanho() {
+        return cboTamanho;
+    }
+
+    public static void setTamanho(String cboTamanho) {
+        BuscarDoProduto.cboTamanho=cboTamanho;
+    }
+
+
+//Preço
+    public static String getPreco() {
+        return cboPreco;
+    }
+    
+    public static void setPreco(String cboPreco) {
+        BuscarDoProduto.cboPreco=cboPreco;
+    }
+
+
+//Marca
+   public static String getMarca() {
+        return cboMarca;
+    }
+
+   public static void setMarca(String cboMarca) {
+        BuscarDoProduto.cboMarca=cboMarca;
+    }
+
+}
